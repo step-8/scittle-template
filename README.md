5ae3f9ade1f0e02f72066a4a29d009e58e7c4503
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2023-06-16 20:12:13 +0530

    Simple scittle template

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scittle template
+
+1. `bb run dev`
+2. [Click here](http://localhost:8000)
+3. In VSCode `cmd-shift-p` and then select `Connect to a running REPL server in the project` and use `1339` as the port when prompted.
---
+++ main.clj
@@ -0,0 +1,11 @@
+(ns main)
+
+(js/alert "It worked")
+
+(defn show-current-time [app]
+  (let [time-tag (.createElement js/document "time")
+        now (js/Date.)]
+    (set! (.-innerText time-tag) (str now))
+    (.appendChild app time-tag)))
+
+(show-current-time (.querySelector js/document "#app"))
